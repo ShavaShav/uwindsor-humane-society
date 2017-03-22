cea381c12a34c488fafb5cc4cf5ed24e32e4b0e1
Ryan <laureau@uwindsor.ca>
2017-03-22 01:44:12 -0400

    complete directory finished

---
+++ public_html/adopt.php
@@ -0,0 +1,91 @@
+<?php
+require_once('lib/config.php');
+require_once('lib/common.php');
+
+html5_navigation(
+	'Adopt Animals',
+	array('nav.css', 'adopt.css', 'background.css'),
+	array(),
+	"is logged in: ".is_logged_in()
+);
+?>
+	<background> </background>
+	
+	<div id="formspace">
+		<form id="ourForm">
+			<label>Animal:</label>
+				<select>
+					<option>Dog</option>
+					<option>Cat</option>
+					<option>Rabbit</option>
+					<option>Hamster</option>
+				</select>
+			<label>Age:</label>
+				<select>
+					<option>0</option>
+					<option>1</option>
+					<option>2</option>
+					<option>3</option>
+					<option>4</option>
+					<option>5</option>
+					<option>6</option>
+					<option>7</option>
+					<option>8</option>
+					<option>9</option>
+					<option>10</option>
+					<option>11</option>
+					<option>12</option>
+					<option>13</option>
+					<option>14</option>
+					<option>15</option>
+					<option>16</option>
+					<option>17</option>
+					<option>18</option>
+					<option>19</option>
+					<option>20+</option>
+				</select>
+			<label>Gender:</lavel>
+				<select>
+					<option>Male</option>
+					<option>Female</option>
+				</select>
+			<label>Spayed/Neutered?</label>
+				<select>
+					<option>Yes</option>
+					<option>No</option>
+				</select>
+			<label>Size:</label>
+				<select>
+					<option>Very Small</option>
+					<option>Small</option>
+					<option>Medium</option>
+					<option>Large</option>
+					<option>Very Large</option>
+				</select>
+			<label>Primary Color</label>
+				<select>
+					<option>Black</option>
+					<option>Grey</option>
+					<option>Yellow</option>
+					<option>White</option>
+					<option>Brown</option>
+				</select>
+			<label>Secondary Color</label>
+				<select>
+					<option>Black</option>
+					<option>Grey</option>
+					<option>Yellow</option>
+					<option>White</option>
+					<option>Brown</option>
+				</select>
+				
+		</form>
+	</div>
+	
+	<div id="resultspace">
+	</div>
+	
+<?php
+html5_epilog();
+?>
+	
--- public_html/index.php
+++ public_html/index.php
@@ -1,39 +1,44 @@
 <?php
+require_once('lib/config.php');
+session_start();
 require_once('lib/common.php');
 
-html5_prolog(
-  'University of Windsor Humane Society',
-  array('background.css', 'buttons.css'),
-  array(),
+html5_index(
+	'University of Windsor Humane Society',
+	array('buttons.css', 'background.css'),
+	array(),
+	"is logged in: ".is_logged_in()
 );
 
 ?>
-	<background></background>
-			
-		<div class="buttons">
-			<div id="About" style="cursor:pointer;" 
-			onclick="document.location='About.html'"> 
-				<div><p>About Us</p></div>
-			</div>
-			
-			
-			<div id="Animals" style="cursor:pointer;" 
-			onclick="document.location='AdoptAnimal.php'">
-				<div><p>Adopt Animals</p></div>
-			</div>
-			
-			<div id="Surrender" style="cursor:pointer;" 
-			onclick="document.location='SurrenderAnimals.php'">
-				<div><p>Surrender Animals</p></div>
-			</div>
-			
-			<div id="Cruelty" style="cursor:pointer;" 
-			onclick="document.location='Cruelty.php'">
-				<div><p>Report Cruelty</p><div>
-			</div>
-	
+
+<div id="background"> </div>
+
+<div id="buttonborder">
+	<div class="buttons">
+		<div id="About" style="cursor:pointer;" 
+		onclick="document.location='contactus.php'"> 
+			<div><p>Contact Us</p></div>
 		</div>
+				
+				
+		<div id="Animals" style="cursor:pointer;" 
+		onclick="document.location='adopt.php'">
+			<div><p>Adopt Animals</p></div>
+		</div>
+				
+		<div id="Surrender" style="cursor:pointer;" 
+		onclick="document.location='surrender.php'">
+			<div><p>Surrender Animals</p></div>
+		</div>
+				
+		<div id="Cruelty" style="cursor:pointer;" 
+		onclick="document.location='cruelty.php'">
+			<div><p>Report Cruelty</p><div>
+		</div>
+	</div>
+</div>
 
 <?php
-html5_epilogue();
+html5_epilog();
 ?>
