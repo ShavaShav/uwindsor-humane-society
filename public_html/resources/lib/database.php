22fcec0c6fb41ec26c82a7938ff8a0eff8ed5b98
shaverz <shaverz@uwindsor.ca>
2017-03-25 19:14:21 -0400

    working on db

--- public_html/resources/lib/database.php
+++ public_html/resources/lib/database.php
@@ -1,50 +1,28 @@
 <?php
 require_once(dirname(__FILE__).'/../config.php');
 
-echo $DB["host"];
-echo $DB["username"];
-echo $DB["password"];
-echo $DB["dbname"];
-
-echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>Name</th><th>Species</th><th>Age</th></tr>";
-
-class TableRows extends RecursiveIteratorIterator {
-    function __construct($it) {
-        parent::__construct($it, self::LEAVES_ONLY);
-    }
-
-    function current() {
-        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
-    }
-
-    function beginChildren() {
-        echo "<tr>";
-    }
-
-    function endChildren() {
-        echo "</tr>" . "\n";
-    }
-}
-
-
-try {
+function prepareStatement($statement){
+    try {
+    // connect to db
     $conn = new PDO("mysql:host=".$DB["host"].";dbname=".$DB["dbname"], $DB["username"], $DB["password"]);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT id, name, species, age FROM Animals");
+    
+    // do the query
+    $stmt = $conn->prepare($statement);
     $stmt->execute();
 
     // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-        echo $v;
-    }
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $result;
 }
 catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
-$conn = null;
-echo "</table>";
+    // close current db connection
+    $conn = null;
+}
+
+$result = prepareStatement("SELECT id, name, species, age FROM Animals");
 
 
 
