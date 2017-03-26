027e4276a1525d71ebab9742e157774dbba400a4
shaverz <shaverz@uwindsor.ca>
2017-03-25 21:35:48 -0400

    database stuff

--- public_html/resources/lib/database.php
+++ public_html/resources/lib/database.php
@@ -1,7 +1,13 @@
 <?php
-require_once(dirname(__FILE__).'/../config.php');
+require_once('../config.php');
+
+echo $DB["host"] . "\n";
+echo $DB["dbname"]."\n";
+echo $DB["username"] . "\n";
+echo $DB["password"] . "\n";
 
 function prepareStatement($statement){
+    global $DB;
     try {
     // connect to db
     $conn = new PDO("mysql:host=".$DB["host"].";dbname=".$DB["dbname"], $DB["username"], $DB["password"]);
@@ -22,9 +28,20 @@ catch(PDOException $e) {
     $conn = null;
 }
 
+// make a query
 $result = prepareStatement("SELECT id, name, species, age FROM Animals");
 
+//create an array of the results
+//$resultsArray = array();
+foreach($result as $row){
+    print_r($row);
+    echo "<br>";
+    //array_push($results_array, $row);
+}
 
+// prints json object
+//echo json_encode($resultsarray);
+print('\n');
 
 ?>
 
