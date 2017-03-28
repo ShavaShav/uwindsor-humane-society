dea4db5cc3b2b2eb442bf71cfac7e1f71a71f2d5
Zach <shaverz@uwindsor.ca>
2017-03-28 00:58:29 -0400

    refactored db code a little - seperate class for each table

--- preneys_examples/simple-examples/forms-basic/target.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-header('Content-Type: text/plain');
-
-print_r($_POST);
-
-?>
--- preneys_examples/simple-examples/optional/xslt/xslt/xml.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header('Content-Type: text/xml');
-
-$xmlFile = new DOMDocument();
-$xmlFile->load('/home/preney/public_html/examples/xml-eg1/myfile.xml');
-echo $xmlFile->saveXML();
-
-?>
