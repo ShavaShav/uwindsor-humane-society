069a50ed908c781d9f9d362f1d9582c90707b028
Kim Ficara <kim_ficara@MacBook-Pro-6.local>
2017-03-28 16:00:46 -0400

    finished implementing the Search Animals box in adoption

--- public_html/js/animalSearch.js
+++ public_html/js/animalSearch.js
@@ -4,6 +4,8 @@ window.addEventListener("load", init);
 function init(){
     // add animalSearch handler
     document.getElementById("animalSearchButton").addEventListener("click", animalSearchHandler);
+    document.getElementById("name").addEventListener("keyup", nameSearchHandler);
+    
 }
 
 // handler for search button
@@ -31,4 +33,41 @@ function animalSearchHandler(){
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send(param); 
     // send request to store_wishlist
+}
+
+// handler for names starting with
+function nameSearchHandler(){
+    /* stop form from submitting normally */
+    event.preventDefault(); 
+    
+    var input = document.getElementById("name").value;
+
+    /* get the filter options from the page for POST */
+    var param = 'name='+document.getElementById("name").value;
+    
+    if (input.length == 0)
+    {
+    	document.getElementById("autoSuggestion").innerHTML="Type for suggestion!";
+    }
+    
+    else
+    {
+    	 // AJAX -> ask server (filter_handler.php) to grab animals from db
+    	var xhttp = new XMLHttpRequest(); // make http request obj
+    	
+    	// When response received, this is called
+    	xhttp.onreadystatechange = function(){
+    	// if request finished, and everything OK
+    	if (this.readyState == 4 && this.status == 200){
+    		// place content returned from fitler_handler in animalBox
+    		document.getElementById("autoSuggestion").innerHTML=this.responseText;
+        		}
+    	}
+
+    /* Send the data and write the results to the animal box */
+    xhttp.open("POST", "resources/lib/nameSearch_handler.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(param); 
+    // send request to store_wishlist
+    }
 }
---
+++ public_html/resources/lib/nameSearch_handler.php
@@ -0,0 +1,24 @@
+<?php 
+
+require_once('database.php');
+
+// go through POST fields(filters), building array to make query
+$partialSearch = $_POST[name];
+
+$db = new AnimalDB; // connect to database
+
+ $animals = $db->getNamesStartingWith($partialSearch); // query db to get array of animals
+ 
+ if (sizeof($animals) != 0)
+ {
+ 	echo $animals[0][name];
+ }
+ 
+ else
+ {
+ 	echo "No animals found!";
+ }
+ 
+ echo "<script>makeAnimalsDraggable()</script>";
+
+?>
