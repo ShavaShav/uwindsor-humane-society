2b9ea672263cdc3b75da67480ad4fa4d7e1be43b
ShavaShav <zachshaver@gmail.com>
2017-04-03 01:20:27 -0400

    admin page done, emails are sent on confirm/denial

--- public_html/resources/lib/pending_handler.php
+++ public_html/resources/lib/pending_handler.php
@@ -10,49 +10,116 @@ else if (isset($_POST['reload_surrenders']))
 {
     reloadSurrenders();
 } else if (isset($_POST['option'])) {
+    // storing some post variables and common stuff that will be in every email
     $option = $_POST['option'];
+    $admin_email = 'uwindsorhs@gmail.com';
+    // set up mail header   
+    $db = new UserDB;
+    $username = $_POST['username'];
+    $user = $db->lookup($username); // get the user details
+    $user_email = $user['email']; // get their email address
+    $db->close();
+    
+    $header = "From: $admin_email\r\n"; 
+    $header.= "MIME-Version: 1.0\r\n"; 
+    $header.= "Content-Type: text/plain; charset=utf-8\r\n"; 
+    $header.= "X-Priority: 1\r\n"; 
+    
+    $message = "Dear ".$username.", \r\n\r\n";
+    
     if (strpos($option, 'surrender') !== false){
         // surrender change
         $db = new SurrenderDB;
+        $surrenderID = $_POST['id'];
+        $surrenderAnimal = $db->lookup($surrenderID);
+        $surrenderName = $surrenderAnimal['name'];
         if (!strcmp($option, 'confirm_surrender')){
             // confirming surrender
             $surrenderID = $_POST['id'];
             $animalID = $db->moveToAnimalsTable($surrenderID);
             // Move image from img/surrenders/ to img/animals/ with new id
             rename('../../img/surrenders/'.$surrenderID.'.jpg', '../../img/animals/'.$animalID.'.jpg');
-            //TODO SEND EMAILS?
+            
+            // send email to user
+            $subject = 'Approval of '.surrenderName.'\'s surrender to the uWindsor Humane Society.';
+            $message .= "Your request for surrender of ".surrenderName." to the uWindsor Humane Society has been approved. Please stop by soon to drop them off and fill out some paperwork. Feel free to contact us if you would like more information.";
         } else if (!strcmp($option, 'deny_surrender')){
             // denying surrender
-            $id = $_POST['id'];
-            $db->remove($id); // remove from surrender table
+            $db->remove($surrenderId); // remove from surrender table
             // delete image
             unlink('../../img/surrenders/' . $id . '.jpg');
-            //TODO SEND EMAILS?
+            
+             // send email to user
+            $subject = 'Denial of '.surrenderName.'\'s surrender to the uWindsor Humane Society.';
+            $message .= "Unfortunately, your request for surrender of ".surrenderName." to the uWindsor Humane Society has been denied. Please contact us if you would like more information.";
         }
         $db->close();
         reloadSurrenders();
     } else {
         // adoption change
         $db = new AnimalDB;
+        $id = $_POST['id'];
+        $animal = $db->lookup($id);
+        $animalName = $animal['name'];
         if (!strcmp($option, 'confirm_adoption')){
-            // confirming adoption
- // COMMENTING OUT FOR NOW UNTIL WE GET SURRENDER UP SO WE CAN REINSERT THEM!!!
-            $animal_id = $_POST['id'];
-   //         $db->remove($animal_id);
+            // confirming adoption, Send email to users
+            // will need to email other users who were effectively denied
+            $users = $db->getUsersWhoRequestAdoption($id);// get all users who want this animal
+            foreach($users as $user_entry){
+                if (!strcmp($user_entry['username'], $username)) {
+                    // the chosen user who was confirmed.
+                    $subject = $animalName.' has got a new home!';
+                    $message .= "Your request to adopt ".$animalName." from the uWindsor Humane Society has been approved. Please come by soon to fill out paperwork and pick them up! :) Please contact us if you would like more information.";
+                    // this will be in the main email being generated here
+                } else {
+                    // denied users, call function that makes a seperate email
+                    adoptDenyEmail($user_entry, $animalName, $header);
+                }
+            }
+ // COMMENTING OUT FOR NOW UNTIL WE GET SURRENDER UP SO WE CAN REINSERT THEM!!!    
+            // now safe to remove the animal from our tables
+//            $db->remove($id);
             // delete image
- //           unlink('../../img/animals/' . $animal_id . '.jpg');
-            //TODO SEND EMAILS?
+//            unlink('../../img/animals/' . $id . '.jpg');
         } else if (!strcmp($option, 'deny_adoption')){
             // denying adoption
-            $username = $_POST['username'];
-            $animal_id = $_POST['id'];
             // remove from adoption table
-            $db->denyAdoption($username, $animal_id);
-            //TODO SEND EMAILS?
+            $db->denyAdoption($username, $id);
+            
+            // Send email to user
+            $subject = 'Denial of '.$animalName.'\'s adoption from the uWindsor Humane Society.';
+            $message .= "After much consideration, your request to adopt ".$animalName." from the uWindsor Humane Society has been denied. We would like to thank you for your interest. Please contact us if you would like more information.";
         }
         $db->close();
         reloadAdoptions();
     }
+    $message .= "\r\n\r\nSincerely,\r\nThe uWindsor Humane Society Team";
+    
+    $success = mail($user_email, $subject, $message, $header);
+    
+    // show the admin whether operations were successful via JS window alert
+    if ($success){
+        echo '<script>alert("Users have been notified by email!")</script>';
+    } else {
+        echo '<script>alert("Postmaster was unable to send emails!")</script>';
+    }
+    
+}
+
+// this method does not check for if a denial letter has error in sending
+function adoptDenyEmail($user, $animalName, $header){
+    // set up mail header   
+    $db = new UserDB;
+    $username = $_POST['username'];
+    $user = $db->lookup($username); // get the user details
+    $user_email = $user['email']; // get their email address
+    $db->close();
+    
+    // build email 
+    $subject = $animalName.' is no longer available for adoption from the uWindsor Humane Society.';
+    $message .= "Unfortunately, ".$animalName." is not longer available for adoption from the uWindsor Humane Society. We would like to thank you for your interest. Please contact us if you would like more information.";
+
+    $message .= "\r\n\r\nSincerely,\r\nThe uWindsor Humane Society Team";
 }
 
 // print out admin divs based on Adoptions Table
