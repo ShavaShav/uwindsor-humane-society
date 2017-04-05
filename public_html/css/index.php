08fc4a787105951987cf628cc3f8db7c6ef58e1e
ShavaShav <zachshaver@gmail.com>
2017-04-05 01:21:31 -0400

    fixing layout bugs

---
+++ public_html/css/index.php
@@ -0,0 +1,13 @@
+<?php
+echo <<<ZZEOF
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>403 Forbidden</title>
+</head><body>
+<h1>Forbidden</h1>
+<p>You don't have permission to access this directory. Nice try!! :)<br />
+</p>
+<hr>
+</body></html>
+ZZEOF;
+?>
