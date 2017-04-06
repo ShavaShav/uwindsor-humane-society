b692f708358031bb319d3f28049e80f7bcf03e87
Zach <shaverz@uwindsor.ca>
2017-04-05 20:23:41 -0400

    added time-based greeting message

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -2,13 +2,23 @@ window.addEventListener("load", function() {
     var d = new Date();
     var text = "";
     
-    if (d.getHours() < 6)
+    var box = document.getElementById("greetingBox");
+    
+    if (d.getHours() < 6){
+        box.className = "night";
         text = "<b>You should be sleeping! ;)</b>";
-    else if (d.getHours() < 12)
+    } else if (d.getHours() < 12) {
+        box.className = "morning";
         text = "<b>Good Morning!</b>";
-    else if (d.getHours() < 18)
+    }
+    else if (d.getHours() < 18){
+        box.className = "night";
         text = "<b>Good Afternoon!</b>";
-    else
+    }
+    else {
+        box.className = "evening";
         text = "<b>Have a good evening!</b><br> We are currently closed, but our website is always open!";
+    }
+    
     document.getElementById("greetingMsg").innerHTML=text;
 });
